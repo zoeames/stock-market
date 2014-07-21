d1557153d8a041fa4f4e9b929c8a7e04ba8ee289
Shruti Sharma <shrutijalewar@gail.com>
2014-07-21 14:29:40 -0500

    add stock to portfolio

--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -1,11 +1,31 @@
 
 'use strict';
 
-//var request = require('request');
+var request = require('request');
+var Stock = require('./stock.js');
 
 function Portfolio(name){
   this.name = name;
   this.stocks = [];
 }
 
+Portfolio.prototype.add = function(symbol, amount){
+  var stock = findStock(this.stocks,symbol);
+  if(stock){
+    stock.count += amount;
+  }else{
+    stock = new Stock(symbol,amount);
+    this.stocks.push(stock);
+}
+};
+//helper function//
+
+function findStock(stocks,symbol){
+  for(var i=0; i<stocks.length; i++){
+  if(stocks[i].symbol ===symbol.toUpperCase()){
+    return stocks[i];
+  }
+  }
+return null;
+}
 module.exports = Portfolio;
--- test/unit/portfolio.js
+++ test/unit/portfolio.js
@@ -4,6 +4,7 @@
 
  var expect = require('chai').expect;
  var Portfolio = require('../../app/models/portfolio.js');
+ var Stock = require('../../app/models/stock');
 
  describe('Portfolio', function(){
    describe('constructor', function(){
@@ -15,5 +16,18 @@
        expect(orange.stocks).to.be.a('array');
        expect(orange.stocks).to.have.length(0);
         });
+      });
+   describe('#add', function(){
+     it('Should add a stock to the portfolio', function(){
+       var tech = new Portfolio('tech');
+       tech.add('appl',50);
+       tech.add('AMZN',30);
+       tech.add('appl',50);
+
+       expect(tech.stocks).to.have.length(2);
+       expect(tech.stocks[0]).to.be.instanceof(Stock);
+       expect(tech.stocks[0].count).to.equal(100);
+       expect(tech.stocks[1].count).to.equal(30);
+     });
       });
  });
